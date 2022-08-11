65c1b20c85edfe8d3ff43a849fc6982b55dc9627
Sofie <stk@umbraco.com>
2022-08-11 10:28:42 +0200

    Another attempt at nav

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,3 +24,7 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'navigation_depth': 4,
+    'prev_next_buttons_location': 'bottom'
+}
